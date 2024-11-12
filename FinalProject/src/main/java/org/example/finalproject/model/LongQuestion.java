37f89914df81b5a9072e765114cee8fab0ef563b
4lex16 <93353749+4lex16@users.noreply.github.com>
2024-11-11 20:30:36 -0500

    Implemented the Question class and it's subclasses - Includes createQuestion class which returns a question based on what type of answer was provided

--- FinalProject/src/main/java/org/example/finalproject/model/LongQuestion.java
+++ FinalProject/src/main/java/org/example/finalproject/model/LongQuestion.java
@@ -1,4 +1,22 @@
 package org.example.finalproject.model;
 
-public class LongQuestion {
+public class LongQuestion extends Question {
+    private String answer;
+
+    public LongQuestion(String question) {
+        super(question);
+    }
+
+    public LongQuestion(String question, String answer) {
+        super(question);
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
