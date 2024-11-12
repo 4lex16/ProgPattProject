856fb8dc2330e439e740b8398f93472f1a1b9012
4lex16 <93353749+4lex16@users.noreply.github.com>
2024-11-12 14:22:55 -0500

    AT-3: Implemented Exam class and subclass & Grade class

--- FinalProject/src/main/java/org/example/finalproject/model/Quiz.java
+++ FinalProject/src/main/java/org/example/finalproject/model/Quiz.java
@@ -1,4 +1,17 @@
 package org.example.finalproject.model;
 
-public class Quiz {
+import lombok.Getter;
+
+@Getter
+public class Quiz extends Exam {
+    private int timesTaken;
+
+    public Quiz() {
+        timesTaken = 0;
+    }
+
+    public void taken() {
+        // Maybe lock it?
+        timesTaken++;
+    }
 }
