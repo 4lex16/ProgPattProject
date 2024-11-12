7c0bd4d48a9851da23edf91c08d6b0d3cd324e2e
tarek-rf <tareqrf@gmail.com>
2024-11-11 21:04:17 -0500

    Implemented User class and its subclasses - added lombok dependency to pom.xml

--- FinalProject/src/main/java/org/example/finalproject/model/Student.java
+++ FinalProject/src/main/java/org/example/finalproject/model/Student.java
@@ -1,4 +1,24 @@
 package org.example.finalproject.model;
 
-public class Student {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class Student extends User {
+    List<Test> tests;
+    List<Quiz> quizzes;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email, String password, String type, List<Test> tests, List<Quiz> quizzes) {
+        super(firstName, lastName, email, password, type);
+        this.tests = tests;
+        this.quizzes = quizzes;
+    }
 }
--- FinalProject/src/main/java/org/example/finalproject/model/User.java
+++ FinalProject/src/main/java/org/example/finalproject/model/User.java
@@ -1,5 +1,29 @@
 package org.example.finalproject.model;
 
-public abstract class User {
+import lombok.*;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public abstract class User {
+    protected int userId;
+    protected String firstName;
+    protected String lastName;
+    protected String email;
+    protected String password;
+    protected String type;
+    private static int counter = 0;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password, String type) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.type = type;
+        userId = ++counter;
+    }
 }
