885a8247c8fd8ca270d2e9e52a38372ce8c7223b
4lex16 <93353749+4lex16@users.noreply.github.com>
2024-12-07 16:46:07 -0500

    AT-21: Create Database Controller + database file

--- FinalProject/src/main/java/module-info.java
+++ FinalProject/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.finalproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
+    requires java.sql;
 
 
     opens org.example.finalproject to javafx.fxml;
